9863474e7fa4720d8f2714cef583b2178a57bb46
WALIDSAIFI <saifiwalid21@gmail.com>
2024-09-09 14:58:09 +0100

    config........ date

---
+++ Utilitaire/DateUtils.java
@@ -0,0 +1,21 @@
+package Utilitaire;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateUtils {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public static LocalDate parseDate(String dateStr) {
+        try {
+            return LocalDate.parse(dateStr, formatter);
+        } catch (DateTimeParseException e) {
+            System.out.println("Erreur de format de date. Utilisez le format 'jj/MM/aaaa'.");
+            return null;
+        }
+    }
+
+    public static String formatDate(LocalDate date) {
+        return date.format(formatter);
+    }
+}
