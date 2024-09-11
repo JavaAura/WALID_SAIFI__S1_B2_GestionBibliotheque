66e49b87e6b7afc9eb31041e32a808007b3284da
WALIDSAIFI <saifiwalid21@gmail.com>
2024-09-11 11:52:36 +0100

    update struteur DAO

--- src/DAO/EtudiantDAO.java
+++ src/DAO/EtudiantDAO.java
@@ -1,7 +1,79 @@
 package src.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import src.Métier.Etudiant;
+
 public class EtudiantDAO {
 
+    public void ajouterEtudiant(Etudiant etudiant) {
+        String sql = "INSERT INTO Etudiant (nom, prenom, matricule) VALUES (?, ?, ?)";
+        try (Connection conn = dbConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
+            pstmt.setString(1, etudiant.getNom());
+            pstmt.setString(2, etudiant.getPrenom());
+            pstmt.setString(3, etudiant.getMatricule());
 
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Etudiant obtenirEtudiant(String matricule) {
+        String sql = "SELECT id, nom, prenom, matricule FROM Etudiant WHERE matricule = ?";
+        try (Connection conn = dbConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, matricule);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                int id = rs.getInt("id");
+                String nom = rs.getString("nom");
+                String prenom = rs.getString("prenom");
+                String mat = rs.getString("matricule");
+
+                return new Etudiant(nom, prenom, mat);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public void mettreAJourEtudiant(Etudiant etudiant) {
+        String sql = "UPDATE Etudiant SET nom = ?, prenom = ?, matricule = ? WHERE matricule = ?";
+        try (Connection conn = dbConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, etudiant.getNom());
+            pstmt.setString(2, etudiant.getPrenom());
+            pstmt.setString(3, etudiant.getMatricule());
+            pstmt.setString(4, etudiant.getMatricule());
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void supprimerEtudiant(String matricule) {
+        String sql = "DELETE FROM Etudiant WHERE matricule = ?";
+        try (Connection conn = dbConnection.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, matricule);
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
